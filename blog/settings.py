da0937805f43ca941caae2e469649d9715c68ef1
rayray <raymart777@yahoo.com>
2020-10-04 13:36:50 +0800

    configure DEBUG Security

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,10 @@ LOGIN_URL = 'users:login'
 #heroku settings.
 import django_heroku
 django_heroku.settings(locals())
+
+
+if os.environ.get('DEBUG') == 'True':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'False':
+    DEBUG = False
+
